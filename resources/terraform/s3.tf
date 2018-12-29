be1e7ccfad68fda1baedb144e15c55d13b83a9bc
Joe Green <joe.green0991@gmail.com>
2018-12-29 17:36:28 +0000

    first commit

---
+++ resources/terraform/s3.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket" "blog" {
+  bucket = "${var.domain}"
+  acl    = "public-read"   # Needs to be public for a website
+
+  versioning {
+    enabled = false
+  }
+
+  lifecycle_rule {
+    prefix  = ""
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 7
+    }
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"   # Actually displayed for all 4XX errors
+  }
+
+  tags {
+    Application    = "${var.application}"
+    Owner          = "${var.owner}"
+    Provisioner    = "Terraform"
+    ProvisionerSrc = "${var.provisionersrc}"
+  }
+}
---
+++ resources/terraform/terraform.tfvars
@@ -0,0 +1,14 @@
+# The region to deploy the resources
+region = "eu-west-1"
+
+# This needs to be different for every deployment within a region
+application = "cms-site"
+
+# A link to this repo - will be added as a tag on all resources
+provisionersrc = "github.com/mrjgreen/cms-site"
+
+# The owner - will be added as a tag on all resources
+owner = "Joe Green"
+
+# Set up a key pair in AWS and add the name here
+domain = "joeg.uk"
